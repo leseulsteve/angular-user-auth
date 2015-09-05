874b633b52900210c0cc55b327313cbc2ae079e3
Steve Boisvert <leseulsteve@gmail.com>
2015-09-05 11:41:58 -0400

    Ajout d'une directive de confirmation de mot de passe

--- dist/angular-user-auth.js
+++ dist/angular-user-auth.js
@@ -179,6 +179,41 @@ angular.module('leseulsteve.angular-user-auth')
     });;
 'use strict';
 
+angular.module('leseulsteve.angular-user-auth').directive('passwordVerify', function () {
+  return {
+    require: 'ngModel',
+    scope: {
+      passwordVerify: '='
+    },
+    link: function (scope, element, attrs, ctrl) {
+
+      scope.$watch(function () {
+        var combined;
+
+        if (scope.passwordVerify || ctrl.$viewValue) {
+          combined = scope.passwordVerify + '_' + ctrl.$viewValue;
+        }
+        return combined;
+      }, function (value) {
+        if (value) {
+          ctrl.$parsers.unshift(function (viewValue) {
+            var origin = scope.passwordVerify;
+            if (origin !== viewValue) {
+              ctrl.$setValidity('passwordVerify', false);
+              return undefined;
+            } else {
+              ctrl.$setValidity('passwordVerify', true);
+              return viewValue;
+            }
+          });
+        }
+      });
+    }
+  };
+});
+;
+'use strict';
+
 angular.module('leseulsteve.angular-user-auth').directive('signinForm',
 	['$animate', 'UserAuth', function($animate, UserAuth) {
 		return {
